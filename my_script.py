5f4737d0ef391f9edc2d2685196aa1a9f4c456f0
Sedarkorka <lelevekorka@gmail.com>
2023-12-18 16:13:58 +0100

    add the input promt for url

--- my_script.py
+++ my_script.py
@@ -16,19 +16,24 @@ import os
 
 # Load the dataset
 # Define the URL of the edge list file for the Facebook-Ego network
-url_facebook = os.getenv('FACEBOOK_URL')  
+url_facebook = input('Enter the Facebook URL (or press Enter to use the default):\n')
+if not url_facebook:
+    url_facebook = os.getenv('FACEBOOK_URL', "https://raw.githubusercontent.com/wang422003/Complex-Networks_exercise/main/Datasets/Group3/Facebook-Ego/348.edges")
+
 # Download the Facebook-Ego network file
 urllib.request.urlretrieve(url_facebook, "facebook.edges")
 # Load the Facebook-Ego network (undirected graph)
 facebook_network = nx.read_edgelist("facebook.edges", nodetype=int)
 
 # Define the URL of the edge list file for the Twitter-Ego network
-url_twitter = os.getenv('TWITTER_URL')  
+url_twitter = input('Enter the Twitter URL (or press Enter to use the default):\n')
+if not url_twitter:
+    url_twitter = os.getenv('TWITTER_URL', "https://raw.githubusercontent.com/wang422003/Complex-Networks_exercise/main/Datasets/Group3/Twitter-Ego/789071.edges")
+
 # Download the Twitter-Ego network file
 urllib.request.urlretrieve(url_twitter, "twitter.edges")
 # Load the Twitter-Ego network (directed graph)
 twitter_network = nx.read_edgelist("twitter.edges", nodetype=int, create_using=nx.DiGraph())
-
 
 ##Question a
 # Get the number of nodes and edges in the Facebook-Ego network
@@ -41,10 +46,10 @@ twitter_num_edges = len(twitter_network.edges())
 
 # Print the results for Facebook-Ego network
 print("For Facebook-Ego Network:")
-print("The number of nodes is : ", facebook_num_nodes)
-print("The number of edges is : ", facebook_num_edges)
+print("The number of nodes is:", facebook_num_nodes)
+print("The number of edges is:", facebook_num_edges)
 
 # Print the results for Twitter-Ego network
 print("For Twitter-Ego Network:")
-print("The number of nodes is : ", twitter_num_nodes)
-print("The number of edges is : ", twitter_num_edges)
+print("The number of nodes is:", twitter_num_nodes)
+print("The number of edges is:", twitter_num_edges)
